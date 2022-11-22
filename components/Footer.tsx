be3bc7dff6f4fbc4b0534970f3132fd6deab3555
doinghun <de.qtner@gmail.com>
2022-11-22 11:06:58 +0800

    remove youtube

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -41,9 +41,6 @@ export default function Footer() {
             Twitter
           </ExternalLink>
           <ExternalLink href="https://github.com/doinghun">GitHub</ExternalLink>
-          <ExternalLink href="https://www.youtube.com/channel/UCZMli3czZnd1uoc1ShTouQw">
-            YouTube
-          </ExternalLink>
         </div>
         <div className="flex flex-col space-y-4">
           <Link
