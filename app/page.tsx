90a8105be280faec952108aec6f0bc35bcbe6640
dohun <de.qtner@gmail.com>
2025-08-05 10:32:58 +0800

    fix: correct punctuation in introduction text on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         Dohun Kim
       </h1>
       <p className="mb-4">
-        {`Hi, I’m Dohun. I build products—fast. I started in mechanical engineering, moved into marketing, and now I write code and ship software. I’ve worked at big companies and small startup, but what I enjoy most is building things people actually use. This blog is where I share what I’ve learned, what I’m working on or just my organized thoughts.`}
+        {`Hi, I’m Dohun. I build products, fast. I started in mechanical engineering, moved into marketing, and now I write code and ship software. I’ve worked at big companies and small startup, but what I enjoy most is building things people actually use. This blog is where I share what I’ve learned, what I’m working on or just my organized thoughts.`}
       </p>
       <div className="my-8">
         <BlogPosts />
